95bd27ec70d5c25280acfb1d40c08b63a8e5c219
Sebas <sebasverac331@gmail.com>
2025-05-20 20:46:16 -0600

    feat: remove DefaultSaltSize constant from SecurityConstants

--- backend/Constants/SecurityConstants.cs
+++ backend/Constants/SecurityConstants.cs
@@ -3,6 +3,5 @@ namespace backend.Constants
     public static class SecurityConstants
     {
         public const int MaxFailedAttempts = 5;
-        public const int DefaultSaltSize = 16;
     }
 }
